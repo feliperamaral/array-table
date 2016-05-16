dd1b304e82951e0b6bb8fbbcd66bc08fb2b0e075
Felipe Amaral <feliperamaral80@gmail.com>
2016-05-16 17:50:56 -0300

    Refatorado todo o código
    Adicionado novas opções
    As opções agora estão mais flexíveis
    Criado nova forma de renderizar os tbody, thead e tfoot
    Adicionado callback global
    Os callback agora são apenas com parametros requeridos

--- src/Fee/ArrayTable.php
+++ src/Fee/ArrayTable.php
@@ -6,33 +6,38 @@ class ArrayTable
 {
 
     /**
-     * $options = array(
-     *   'callbacks' => array(
-     *       'fields' => array(
-     *       'telefone' => function($filedValue, $row) {
-     *           //code
-     *              return $filedValue;
-     *           }
-     *       )
-     *   )
-     * );
-     *
-     *
      * @param array $data
      * @param array $nameCols
      * @param array $options
      * @return string
      */
     private static $funcsNumArgs = array();
+    private static $commomDefault = array(
+        'attributes' => array(),
+        'rowAttributes' => array(),
+        'cellAttributes' => array(),
+        'callbacks' => array(
+            '*' => null,
+            'index' => array()
+        ),
+        'values' => array(
+            '*' => null,
+            'index' => array()
+        )
+    );
     public static $optionsDefault = array(
         'table' => array(
-            'attributes' => array(
-                'class' => '',
-            ),
+            'attributes' => array()
         ),
-        'callbacks' => array(
-            'fields' => array()
-        )
+        'thead' => array(
+            'typeOfCell' => 'th'
+        ),
+        'tbody' => array(
+            'typeOfCell' => 'td'
+        ),
+        'tfoot' => array(
+            'typeOfCell' => 'td'
+        ),
     );
 
     public static function render($data, $nameCols = array(), $options = array())
@@ -44,39 +49,45 @@ class ArrayTable
             throw new \InvalidArgumentException('The argument "$data" is not a "array" or instance of "\Traversable". "$data" is: ' . gettype($data));
         }
 
-        $options = array_replace_recursive(self::$optionsDefault, $options);
+        $options = self::normalizeOptions($options);
 
-        $data = array_map(function($value) {
-            if ($value instanceof \Traversable) {
-                $value = iterator_to_array($value);
-            }
-            return $value;
-        }, $data);
-
-        $firstKeys = array_keys(current($data));
-
-        $html = '<table' . self::arrayToAttributes($options['table']['attributes']) . '>';
-        if ($nameCols !== null) {
-
-            if (!$nameCols) {
-                $nameCols = array_combine($firstKeys, $firstKeys);
-            }
-            $html .= '<thead>';
-            $html .= '<tr>';
-            foreach ($nameCols? : $firstKeys as $name => $colName) {
-                $html .= "<th>{$colName}</th>";
-            }
-            $html .= '</tr>';
-            $html .= '</thead>';
-        }
         if (!$nameCols) {
+            $firstKeys = array_keys(current($data));
             $nameCols = array_combine($firstKeys, $firstKeys);
         }
 
-        $html .= '<tbody>';
-
         reset($data);
+
+        $html = '<table' . self::arrayToAttr($options['table']['attributes']) . '>';
+
+        if (sizeof($data) <= 3 && (isset($data['thead']) || isset($data['tbody']) || isset($data['tfoot']))) {
+            foreach ($data as $type => $dataTable) {
+                $html .= self::renderGroup($dataTable, $nameCols, $options[$type], $type);
+            }
+        } else {
+            $html .= self::renderGroup(array($nameCols), $nameCols, $options['thead'], 'thead');
+            $html .= self::renderGroup($data, $nameCols, $options['tbody'], 'tbody');
+        }
+        return $html . '</table>';
+    }
+
+    /**
+     * 
+     * @param array|\Traversable $data
+     * @param array $indices
+     * @param array $options
+     * @param string $type
+     * @return string
+     */
+    private static function renderGroup($data, $indices, $options, $type)
+    {
+
+        $html = "<$type>";
+
         foreach ($data as $row):
+            if ($row instanceof \Traversable) {
+                $row = iterator_to_array($row);
+            }
             $currentConfigs = array(
                 'tr' => array(
                     'attributes' => ''
@@ -84,50 +95,86 @@ class ArrayTable
             );
             $contentTr = '';
 
-            foreach ($nameCols as $name => $colName):
-                $content = isset($row[$name]) ? $row[$name] : '';
-                if (isset($options['values']['fields'][$name])) {
-                    $content = $options['values']['fields'][$name];
-                }
+            foreach ($indices as $index) {
+                $contentTr .= self::renderContent($row, $index, $options);
+            }
 
-                $currentConfigs['td'] = array(
-                    'attributes' => ''
-                );
-                if (isset($options['callbacks']['fields'][$name]) && is_callable($options['callbacks']['fields'][$name])) {
-                    $arguments = array(
-                        &$content, &$row, &$currentConfigs
-                    );
-                    $numArgument = self::getNumArgs($options['callbacks']['fields'][$name]);
-
-                    if ($numArgument !== false && is_int($numArgument)) {
-                        array_splice($arguments, $numArgument);
-                        $result = call_user_func_array($options['callbacks']['fields'][$name], $arguments);
-                    } else {
-                        $result = $options['callbacks']['fields'][$name]($content, $row, $currentConfigs);
-                    }
-
-                    if ($result !== null) {
-                        $content = $result;
-                    }
-                }
-                $attributesTD = '';
-                if ($currentConfigs['td']['attributes']) {
-                    $attributesTD = self::arrayToAttributes($currentConfigs['td']['attributes']);
-                }
-                $contentTr .= "<td{$attributesTD}>{$content}</td>";
-            endforeach;
-
-            $html .= '<tr' . self::arrayToAttributes($currentConfigs['tr']['attributes']) . ">{$contentTr}</tr>";
+            $html .= '<tr' . self::arrayToAttr($currentConfigs['tr']['attributes']) . ">{$contentTr}</tr>";
         endforeach;
 
-        $html .= '</tbody>';
-
-        $html .= '</table>';
-
-        return $html;
+        return $html . "</$type>";
     }
 
-    public static function arrayToAttributes($array, $returnString = true)
+    private static function renderContent($row, $i, $options)
+    {
+        $content = isset($row[$i]) ? $row[$i] : '';
+
+        if (isset($options['values']['fields'][$i])) {
+            $content = $options['values']['fields'][$i];
+        }
+
+        $currentConfigs = $options;
+        $callbacks = array();
+
+        if(isset($options['callbacks']['*'])){
+            $callbacks[] = $options['callbacks']['*'];
+        }
+
+        if (isset($options['callbacks']['fields'][$i])) {
+            $callbacks = array_merge($callbacks, (array) $options['callbacks']['fields'][$i]);
+        }
+        foreach ($callbacks as $callback) {
+            if (!is_callable($callback)) {
+                continue;
+            }
+
+            $arguments = array(&$content, &$row, &$currentConfigs);
+            $numArgument = self::getNumArgs($callback);
+
+            array_splice($arguments, is_int($numArgument) ? $numArgument : 3);
+
+            $result = call_user_func_array($callback, $arguments);
+
+            if ($result !== null) {
+                $content = $result;
+            }
+        }
+        
+//        var_dump($currentConfigs);
+
+        return "<{$currentConfigs['typeOfCell']}" . self::arrayToAttr($currentConfigs['cellAttributes']) . '>'
+                . $content
+                . "</{$currentConfigs['typeOfCell']}>";
+    }
+
+    private static function normalizeOptions($options)
+    {
+        self::$optionsDefault['thead'] += self::$commomDefault;
+        self::$optionsDefault['tbody'] += self::$commomDefault;
+        self::$optionsDefault['tfoot'] += self::$commomDefault;
+
+        foreach (array_keys(self::$optionsDefault['tbody']) as $ktb) {
+            if (isset($options[$ktb])) {
+                if (!isset($options['tbody']) || !is_array($options['tbody'])) {
+                    $options['tbody'] = array();
+                }
+                $options['tbody'][$ktb] = $options[$ktb];
+                unset($options[$ktb]);
+            }
+        }
+
+        foreach (self::$optionsDefault as $key => $optsDefault) {
+            if (isset($options[$key])) {
+                $options[$key] = array_replace_recursive($optsDefault, $options[$key]);
+            } else {
+                $options[$key] = $optsDefault;
+            }
+        }
+
+        return $options;
+    }
+
+    public static function arrayToAttr($array, $returnString = true)
     {
         if (!is_array($array)) {
             return $array ? ' ' . $array : '';
@@ -152,14 +199,15 @@ class ArrayTable
         if (is_string($func) && isset(self::$funcsNumArgs[$func])) {
             return self::$funcsNumArgs[$func];
         }
-        if (!class_exists('ReflectionFunction'))
+        if (!class_exists('ReflectionFunction')) {
             return false;
+        }
 
         $ref = new \ReflectionFunction($func);
-        if (is_string($func)) {
-            return (self::$funcsNumArgs[$func] = $ref->getNumberOfParameters());
-        }
-        return $ref->getNumberOfParameters();
-    }
 
+        if (is_string($func)) {
+            return (self::$funcsNumArgs[$func] = $ref->getNumberOfRequiredParameters());
+        }
+        return $ref->getNumberOfRequiredParameters();
+    }
 }
